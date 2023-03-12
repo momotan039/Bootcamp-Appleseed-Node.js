15b253922ce2e3c5a0f4d1c997e38ebcd0e061e8
momotan <momotaha039@gmail.com>
2023-03-12 17:17:05 +0200

    file system

---
+++ file system/index.js
@@ -0,0 +1,5 @@
+const fs=require('fs')
+// fs.writeFileSync('file.text','hello in fs Module')
+// fs.copyFileSync('./file.text','copyFile.text')
+// fs.renameSync('file.text','file2.text')
+fs.lstat
