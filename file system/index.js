f4cba2eb4115a1bdc91c344a46aba70ecb47eb43
momotan <momotaha039@gmail.com>
2023-03-15 10:50:05 +0200

    file system

--- file system/index.js
+++ file system/index.js
@@ -2,4 +2,11 @@ const fs=require('fs')
 // fs.writeFileSync('file.text','hello in fs Module')
 // fs.copyFileSync('./file.text','copyFile.text')
 // fs.renameSync('file.text','file2.text')
-fs.lstat
+// fs.readdir('.',(err,files)=>{
+//     if(err)
+//     console.log(err);
+//     console.log(files);
+// })
+
+const content=fs.readFileSync('file2.text','base64')
+console.log(content);
