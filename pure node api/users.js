bb1cd10843bb0fdc9ee235c31764f88377792cff
MOHAMMED <momotaha039@gmail.com>
2023-03-15 23:04:35 +0200

    pure api

---
+++ pure node api/users.js
@@ -0,0 +1,17 @@
+[
+    {
+        id:1,
+        name:'mohammed',
+        age:22
+    },
+    {
+        id:2,
+        name:'rami',
+        age:25
+    },
+    {
+        id:1,
+        name:'khalid',
+        age:50
+    },
+]
